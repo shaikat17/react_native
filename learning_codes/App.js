551dc779f8feec8315010ae45e3ea404b6397801
shaikat17 <shaikatpal56@gmail.com>
2023-02-07 12:26:29 +0600

    FlatList with scroll false

--- learning_codes/App.js
+++ learning_codes/App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import FlatListDemo from '../learning_codes/src/FlatListDemo';
+import FlatListDemoScroll from './src/FlatListScroll';
 
 export default function App() {
   return (
     <View style={styles.container}>
     <Text style={styles.textStyle}>Hello World!!!</Text>
       <StatusBar style="auto" />
-      <FlatListDemo />
+      
+      <FlatListDemoScroll />
     </View>
   );
 }
